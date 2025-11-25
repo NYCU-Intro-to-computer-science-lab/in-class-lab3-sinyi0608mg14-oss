6eaaf5da51d83b4c4a290e99fad434b008be97f1
sinyi0608mg14-oss <sinyi0608.mg14@nycu.edu.tw>
2025-11-25 14:30:39 +0800

    3

--- src/p4.cpp
+++ src/p4.cpp
@@ -2,12 +2,39 @@
 
 #include <iostream>
 using namespace std;
-int fib(int n){
-    if(n<2)return 1;
-    return fib(n-1)+fib(n-2);
+
+int fib(int n, int depth) {
+    printIndent(depth);
+    cout << "SEARCH fib(" << n << ")\n";
+
+    int result;
+    if (n == 1 || n == 2) {
+        result = 1;
+    } 
+    else {
+        int a = fib(n - 1, depth + 1); 
+        int b = fib(n - 2, depth + 1); 
+        result = a + b;
+    }
+
+    printIndent(depth);
+    cout << "GET fib(" << n << ") = " << result << "\n";
+
+    return result;
 }
+void printIndent(int depth) {
+    if (depth <= 0) return; 
+    cout << "|--";
+    printIndent(depth - 1);  
+}
+
+
+
 int main() {
-    
-    
+    int n;
+    cin >> n;
+    int ans = fib(n, 0);
+    cout << ans << endl;
     return 0;
 }
+
--- src/p5.cpp
+++ src/p5.cpp
@@ -3,8 +3,10 @@
 
 using namespace std;
 void hanoi(int n, int from,int to,int aux){
+    hanoi(n,1,2,3);
     if(n==1)
-        cout<<"Move disk"<<n<<"from";
+        cout<<"Move disk"<<n<<"from"<<from<<"to"<<to<<endl;
+    hanoi();
 }
 
 int main() {
