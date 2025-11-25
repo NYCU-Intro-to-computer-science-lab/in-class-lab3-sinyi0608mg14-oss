1c4224c30267dff511e17132c69b2a278f71dd41
sinyi0608mg14-oss <sinyi0608.mg14@nycu.edu.tw>
2025-11-25 14:01:44 +0800

    1

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,9 +2,19 @@
 
 #include <iostream>
 using namespace std;
-
+long long factorial(int n){
+    if(n==0)return 1;
+    return factorial(n)*factorial(n-1);
+}
+long long sum(int n){
+    if(n==0)return 0;
+    return sum(n)+sum(n-1);
+}
 int main() {
-    
+    int a;
+    cin>>a;
+    cout<<factorial(a)<<endl;
+    cout<<sum(a)<<endl;
     
     return 0;
 }
--- src/p4.cpp
+++ src/p4.cpp
@@ -2,7 +2,10 @@
 
 #include <iostream>
 using namespace std;
-
+int fib(int n){
+    if(n<2)return 1;
+    return fib(n-1)+fib(n-2);
+}
 int main() {
     
     
